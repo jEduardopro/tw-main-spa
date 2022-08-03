a99f1e042cb6983329712092ce75ff300b6a27ab
Jesus Eduardo Facio <efacio@webforcehq.com>
2022-08-03 18:25:14 -0500

    I updated sign up module

--- src/app/modules/sign-up/components/sign-up-stepper-form/steps/step-one/step-one.component.ts
+++ src/app/modules/sign-up/components/sign-up-stepper-form/steps/step-one/step-one.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { SignUp } from '../../../../../../core/models/sign-up.model';
+import { SignUp } from '../../../../models/sign-up.model';
 
 @Component({
 	selector: 'app-step-one',
