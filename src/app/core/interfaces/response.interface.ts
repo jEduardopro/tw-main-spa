d2e4cf2cbd40a4be0c538b9bd8e33504f547ec54
Jesus Eduardo <lalo.codein22@gmail.com>
2023-07-18 07:36:27 -0600

    create response interface

---
+++ src/app/core/interfaces/response.interface.ts
@@ -0,0 +1,30 @@
+
+export interface Response<T> {
+	data:  T;
+	links?: Links;
+	meta?:  Meta;
+}
+
+export interface Links {
+	first: string;
+	last:  string;
+	prev:  string|null;
+	next:  string|null;
+}
+
+export interface Meta {
+	current_page: number;
+	from:         number;
+	last_page:    number;
+	links:        Link[];
+	path:         string;
+	per_page:     number;
+	to:           number;
+	total:        number;
+}
+
+export interface Link {
+	url:    null | string;
+	label:  string;
+	active: boolean;
+}
