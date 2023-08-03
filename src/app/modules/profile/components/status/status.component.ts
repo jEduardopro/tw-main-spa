e8e72997922175d1260e826bf4f39965ddae0894
Jesus Eduardo <lalo.codein22@gmail.com>
2023-08-03 08:08:54 -0600

    read username from store in status component

--- src/app/modules/profile/components/status/status.component.ts
+++ src/app/modules/profile/components/status/status.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Tweet } from '@app/modules/tweets/interfaces/tweet.interface';
 import { TweetService } from '@app/modules/tweets/services/tweet.service';
-import { firstValueFrom } from 'rxjs';
+import { AppState } from '@app/store/app.reducers';
+import { Store } from '@ngrx/store';
+import { Subscription, firstValueFrom } from 'rxjs';
+import { selectProfileUsername } from '../../store/selectors/profile.selectors';
 
 @Component({
   selector: 'app-status',
@@ -10,21 +13,33 @@ import { firstValueFrom } from 'rxjs';
   styles: [
   ]
 })
-export class StatusComponent implements OnInit {
+export class StatusComponent implements OnInit, OnDestroy {
 
 	tweetId: string = ''
 	tweet: Tweet | null = null;
+	username: string| null = null
+	storeSubscription: Subscription = new Subscription
 
 	constructor(
+		private store: Store<AppState>,
 		private route: ActivatedRoute,
 		private tweetService: TweetService
 	) { }
 
 	ngOnInit(): void {
+		const username$ = this.store.select(selectProfileUsername).subscribe(username => {
+			this.username = username || null
+		})
+		this.storeSubscription.add(username$)
+
 		this.route.params.subscribe(params => {
 			this.tweetId = params['tweet']
 			this.getTweet()
 		})
+	}
+
+	ngOnDestroy(): void {
+		this.storeSubscription.unsubscribe()
 	}
 
 	async getTweet() {
@@ -32,10 +47,16 @@ export class StatusComponent implements OnInit {
 			const data = await firstValueFrom(this.tweetService.getTweet(this.tweetId))
 			console.log(data);
 			this.tweet = data
+			this.shouldResolveUsername()
 			
 		} catch (error) {
 			console.log(error);
 		}
+	}
+
+	shouldResolveUsername() {
+		if (this.username !== null) return;
+
 	}
 
 }
