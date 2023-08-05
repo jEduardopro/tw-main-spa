07c8831a3188a9bb769b42a7a480ad997dd90128
Jesus Eduardo <lalo.codein22@gmail.com>
2023-08-05 07:55:00 -0600

    update profile item to hide follow button for auth user

--- src/app/shared/components/profile-item/profile-item.component.ts
+++ src/app/shared/components/profile-item/profile-item.component.ts
@@ -1,15 +1,42 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { selectAuthUserId } from '@app/modules/auth/store/selectors/auth.selectors';
 import { Profile } from '@app/modules/profile/interfaces/profile.interface';
+import { AppState } from '@app/store/app.reducers';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile-item',
   templateUrl: './profile-item.component.html',
   styles: []
 })
-export class ProfileItemComponent {
+export class ProfileItemComponent implements OnInit, OnDestroy {
 
 	@Input() profile!: Profile
-	@Output() follow = new EventEmitter<{id: string, value: boolean}>()
+	@Output() follow = new EventEmitter<{ id: string, value: boolean }>()
+
+	userId = ''
+	storeSubscription: Subscription = new Subscription
+	
+	constructor(
+		private store: Store<AppState>
+	) { }
+
+	ngOnInit(): void {
+		const userId$ = this.store.select(selectAuthUserId).subscribe(id => {
+			this.userId = id
+		})
+
+		this.storeSubscription.add(userId$)
+	}
+
+	ngOnDestroy(): void {
+		this.storeSubscription.unsubscribe()
+	}
+
+	get isAuthUser() {
+		return this.userId === this.profile.id
+	}
 
 	setFollow(id: string, value: boolean) {
 		this.follow.emit({id, value})
