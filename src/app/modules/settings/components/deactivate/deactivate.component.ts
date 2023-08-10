0c25cd80b32aaab223606e1c131c7f8fbe6dc9f9
Jesus Eduardo <lalo.codein22@gmail.com>
2023-08-10 07:45:31 -0600

    update html of deactivate component

--- src/app/modules/settings/components/deactivate/deactivate.component.ts
+++ src/app/modules/settings/components/deactivate/deactivate.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { User } from '@app/modules/auth/interfaces/user.interface';
+import { selectAuthUser } from '@app/modules/auth/store/selectors/auth.selectors';
+import { AppState } from '@app/store/app.reducers';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-deactivate',
@@ -6,9 +11,25 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class DeactivateComponent {
+export class DeactivateComponent implements OnInit, OnDestroy {
+
+	storeSubscription: Subscription = new Subscription
+	authUser!: User
 
 	constructor(
+		private store: Store<AppState>,
 	) { }
+
+	ngOnInit(): void {
+		const authUser$ = this.store.select(selectAuthUser).subscribe(authUser => {
+			this.authUser = authUser
+		})
+
+		this.storeSubscription.add(authUser$)
+	}
+
+	ngOnDestroy(): void {
+		this.storeSubscription.unsubscribe()
+	}
 
 }
